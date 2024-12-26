d7b1e1e7c192c2a66d21111d4f8b3c644e59aa1f
DevWithAbarna <abarnasri9977@gmail.com>
2024-12-26 13:33:38 +0400

    Add project files.

---
+++ YtBookShop/Controllers/BookController.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using YtBookStore.Models.Domain;
+using YtBookStore.Repositories.Abstract;
+
+namespace YtBookStore.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly IAuthorService authorService;
+        private readonly IBookService bookService;
+        private readonly IPublisherService publisherService;
+        private readonly IGenreService genreService;
+        public BookController(IBookService bookService, IAuthorService authorService  ,IPublisherService publisherService, IGenreService genreService)
+        {
+            this.bookService = bookService;
+            this.publisherService = publisherService;
+            this.genreService = genreService;
+            this.authorService = authorService;
+        }
+        public IActionResult Add()
+        {
+            var model=new Book();
+            model.AuthorList = authorService.GetAll().Select(a => new SelectListItem { Text=a.AuthorName,Value=a.Id.ToString()}).ToList();
+            model.PublisherList = publisherService.GetAll().Select(a => new SelectListItem { Text = a.PublisherName, Value = a.Id.ToString() }).ToList();
+            model.GenreList = genreService.GetAll().Select(a => new SelectListItem { Text = a.Name, Value = a.Id.ToString() }).ToList();
+
+            return View(model);
+        }
+        [HttpPost]
+        public IActionResult Add(Book model)
+        {
+            model.AuthorList = authorService.GetAll().Select(a => new SelectListItem { Text = a.AuthorName, Value = a.Id.ToString(),Selected=a.Id==model.AuthorId }).ToList();
+            model.PublisherList = publisherService.GetAll().Select(a => new SelectListItem { Text = a.PublisherName, Value = a.Id.ToString(),Selected=a.Id==model.PublisherId }).ToList();
+            model.GenreList = genreService.GetAll().Select(a => new SelectListItem { Text = a.Name, Value = a.Id.ToString(),Selected=a.Id==model.GnereId }).ToList();
+
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            var result = bookService.Add(model);
+            if (result)
+            {
+                TempData["msg"] = "Added Successfully";
+                return RedirectToAction(nameof(Add));
+            }
+            TempData["msg"] = "Error has occured on server side";
+            return View(model);
+        }
+
+
+        public IActionResult Update(int id)
+        {
+            var model = bookService.FindById(id);
+            model.AuthorList = authorService.GetAll().Select(a => new SelectListItem { Text = a.AuthorName, Value = a.Id.ToString(), Selected = a.Id == model.AuthorId }).ToList();
+            model.PublisherList = publisherService.GetAll().Select(a => new SelectListItem { Text = a.PublisherName, Value = a.Id.ToString(), Selected = a.Id == model.PublisherId }).ToList();
+            model.GenreList = genreService.GetAll().Select(a => new SelectListItem { Text = a.Name, Value = a.Id.ToString(), Selected = a.Id == model.GnereId }).ToList();
+
+            return View(model);
+        }
+        [HttpPost]
+        public IActionResult Update(Book model)
+        {
+            model.AuthorList = authorService.GetAll().Select(a => new SelectListItem { Text = a.AuthorName, Value = a.Id.ToString(), Selected = a.Id == model.AuthorId }).ToList();
+            model.PublisherList = publisherService.GetAll().Select(a => new SelectListItem { Text = a.PublisherName, Value = a.Id.ToString(), Selected = a.Id == model.PublisherId }).ToList();
+            model.GenreList = genreService.GetAll().Select(a => new SelectListItem { Text = a.Name, Value = a.Id.ToString(), Selected = a.Id == model.GnereId }).ToList();
+
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            var result = bookService.Update(model);
+            if (result)
+            {
+                TempData["msg"] = "Updated Successfully";
+                return RedirectToAction("GetAll");
+            }
+            TempData["msg"] = "Error has occured on server side";
+            return View(model);
+        }
+        public IActionResult Delete(int id)
+        {
+            var result = bookService.Delete(id);
+            return RedirectToAction("GetAll");
+
+        }
+        public IActionResult GetAll()
+        {
+            var data = bookService.GetAll();
+            return View(data);
+
+        }
+    }
+}
---
+++ YtBookShop/Repositories/Implementation/GenerService.cs
@@ -0,0 +1,75 @@
+﻿using YtBookStore.Models.Domain;
+using YtBookStore.Repositories.Abstract;
+
+namespace YtBookStore.Repositories.Implementation
+{
+    public class GenerService : IGenreService
+    {
+        private readonly DatabaseContext context;
+
+        public GenerService(DatabaseContext context)
+        {
+            this.context = context;
+        }
+        bool IGenreService.Add(Genre model)
+        {
+            try
+            {
+                context.Genre.Add(model);
+                context.SaveChanges();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+
+        }
+        bool IGenreService.Delete(int id)
+        {
+            try
+            {
+                var data = this.FindById(id);
+                if(data == null) 
+                    return false;
+
+                context.Genre.Remove(data);
+                context.SaveChanges();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        public Genre FindById(int id)
+        {
+            return context.Genre.Find(id);
+        }
+
+        
+
+        IEnumerable<Genre> IGenreService.GetAll()
+        {
+            return context.Genre.ToList();
+        }
+
+        bool IGenreService.Update(Genre model)
+        {
+
+
+            try
+            {
+                context.Genre.Update(model);
+                context.SaveChanges();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+
+        }
+    }
+}
